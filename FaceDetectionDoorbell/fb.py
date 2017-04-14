0ae30b758f02605b684dac22a3f2c464231d0163
bahmanh <bahmanhayat@gmail.com>
2017-04-14 04:05:17 -0700

    facebook upload and delete complete

--- FaceDetectionDoorbell/fb.py
+++ FaceDetectionDoorbell/fb.py
@@ -1,3 +1,23 @@
 import requests 
 from facepy import GraphAPI
+import json
 
+class Facebook(object):
+
+    def __init__(self, token):
+        self.token = token
+        self.graph = GraphAPI(self.token)
+
+    def upload_photo(self, filepath):
+        try:
+            response = self.graph.post(path='me/photos', source=open(filepath, 'rb'))
+        except Exception as e:
+            raise e
+        return response
+
+    def delete_photo(self, post_id):
+        try:
+            response = self.graph.delete(path=post_id)
+        except Exception as e:
+            raise e
+        return response
