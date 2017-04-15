fc8479fafa611ee06042414d14c8c31870da9bb3
bahmanh <bahmanhayat@gmail.com>
2017-04-14 17:07:20 -0700

    complete request to get names from fb

--- FaceDetectionDoorbell/fb.py
+++ FaceDetectionDoorbell/fb.py
@@ -1,5 +1,5 @@
-import requests 
 from facepy import GraphAPI
+import requests 
 import json
 
 class Facebook(object):
@@ -22,11 +22,40 @@ class Facebook(object):
             raise e
         return response
 
-    def recognize_faces(self, filepath):
-        post_id = upload_photo(filepath)['id']
-        delete_photo(post_id)
+    def recognize_faces(self, filepath, cookie, fb_dtsg):
+        post_id = self.upload_photo(filepath)['id']
+        URL = "https://www.facebook.com/photos/tagging/recognition/"
+        headers = {
+                'accept':'*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language':'en-US,en;q=0.8',
+                'content-type':'application/x-www-form-urlencoded',
+                'cookie':'l',
+                'origin':'https://www.facebook.com',
+                'referer':'https://www.facebook.com/',
+                'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+                'x_fb_background_state':'1'
+        }
+        payload = {
+                'dpr':'1',
+                'recognition_project':'composer_facerec',
+                'photos[0]':post_id,
+                'target':'',
+                'is_page':'false',
+                'include_unrecognized_faceboxes':'false',
+                'include_face_crop_src':'false',
+                'include_recognized_user_profile_picture':'false',
+                'include_low_confidence_recognitions':'true',
+                '__a':'1',
+                'fb_dtsg': fb_dtsg
+            }
 
+        response = requests.post(URL, headers=headers, data=payload)
+        names = self.parse_names(response.text)
+        self.delete_photo(post_id)
+        
 
     def parse_names(fb_response):
+        return
 
 
