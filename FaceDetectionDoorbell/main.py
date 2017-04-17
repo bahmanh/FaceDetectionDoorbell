b55f81c9f2c5c966c7031261e2479e6f2ce162ad
bahmanh <bahmanhayat@gmail.com>
2017-04-17 01:36:24 -0700

    Dash button compatibility

--- FaceDetectionDoorbell/main.py
+++ FaceDetectionDoorbell/main.py
@@ -2,23 +2,35 @@ import picamera
 from fb import Facebook
 from messenger import Messenger
 from credentials import FB_KEY, FB_COOKIE, FB_DTSG
+from scapy.all import *
 
-
-phone_number = 0000000000;
+MAC_ADDRESS = '' # enter Dash Button's MAC Address here.
+phone_number = 0000000000 #enter phone number to send text to here. Must be verified with Twilio
 camera = picamera.PiCamera()
-camera.capture('image.jpg')
 
-fb = Facebook(FB_KEY)
-names = fb.recognize_faces('image.jpg', FB_COOKIE, FB_DTSG)
+def transaction():
+    camera.capture('image.jpg')
+    print "Photo taken"
 
-message = '\n'.join(names.keys())
+    fb = Facebook(FB_KEY)
+    names = fb.recognize_faces('image.jpg', FB_COOKIE, FB_DTSG)
 
-if len(names.keys()) == 1:
-    message += '\nis at the door.'
-elif len(names.keys()) > 1:
-    message += '\nare at the door'
-elif len(names.keys()) == 0:
-    message += 'Unrecognized person(s) at the door'
+    message = '\n'.join(names.keys())
 
-mess = Messenger()
-mess.send_text(message, phone_number)
+    if len(names.keys()) == 1:
+        message += '\nis at the door.'
+    elif len(names.keys()) > 1:
+        message += '\nare at the door'
+    elif len(names.keys()) == 0:
+        message += 'Unrecognized person(s) at the door'
+
+    print message
+    mess = Messenger()
+    mess.send_text(message, phone_number)
+
+def detect_button(pkt):
+    if pkt.haslayer(DHCP) and pkt[Ether].src == MAC_ADDRESS:
+            print "Button Press Detected"
+            transaction()
+
+sniff(prn=detect_button, filter="(udp and (port 67 or 68))", store=0)
